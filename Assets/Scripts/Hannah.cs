d68ee279c5d094df53253e056d1b7662c5744f59
BiggerMac <73235546+BiggerMac@users.noreply.github.com>
2020-10-22 11:33:11 -0400

    Add files via upload

--- Assets/Scripts/Hannah.cs
+++ Assets/Scripts/Hannah.cs
@@ -10,6 +10,12 @@ public class Hannah : MonoBehaviour
     private Vector3 moveDirection = Vector3.zero;
     public int speed;
 
+    public GameObject door1;
+    public GameObject door2;
+    public GameObject door3;
+    public GameObject door4;
+    public GameObject door5;
+
     void Start()
     {
         characterController = GetComponent<CharacterController>();
@@ -21,4 +27,34 @@ public class Hannah : MonoBehaviour
         moveDirection = new Vector3(Input.GetAxis("Horizontal") * speed, Input.GetAxis("Vertical") * speed, 0.0f);
         characterController.Move(moveDirection * Time.deltaTime);
     }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.CompareTag("Key1"))
+        {
+            Destroy(col.gameObject);
+            door1.SendMessage("Unlock");
+        }else if (col.gameObject.CompareTag("Key2"))
+        {
+            Destroy(col.gameObject);
+            door2.SendMessage("Unlock");
+        }
+        else if (col.gameObject.CompareTag("Key3"))
+        {
+            Destroy(col.gameObject);
+            door3.SendMessage("Unlock");
+        }
+        else if (col.gameObject.CompareTag("Key4"))
+        {
+            Destroy(col.gameObject);
+            door4.SendMessage("Unlock");
+        }
+        else if (col.gameObject.CompareTag("Key5"))
+        {
+            Destroy(col.gameObject);
+            door5.SendMessage("Unlock");
+        }
+    }
+
+
 }
