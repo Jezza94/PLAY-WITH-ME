2195d1974c8f57d5d0027fa53376dad5659528ce
BiggerMac <73235546+BiggerMac@users.noreply.github.com>
2020-10-22 11:41:02 -0400

    Add files via upload

---
+++ Assets/Scripts/AnotherLockedDoor.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnotherLockedDoor : MonoBehaviour
+{
+
+    //This script is designed to be paired with the keys Hannah picks up. MAKE SURE THAT THE DOOR THIS SCRIPT IS ATTACHED TO IS BOTH THE ONE
+    //THAT HAS THE COLLIDER THAT'S BLOCKING THE PATH, AND IS MENTIONED IN THE HANNAH.CS FILE
+    void Unlock()
+    {
+        GetComponent<Collider>().enabled = false;
+    }
+}
