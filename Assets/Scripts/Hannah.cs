edcfd4641794cba09e5ac601af2e347d75a76178
BiggerMac <73235546+BiggerMac@users.noreply.github.com>
2020-10-22 08:44:02 -0400

    Add files via upload

---
+++ Assets/Scripts/Hannah.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hannah : MonoBehaviour
+{
+    private float moveX;
+    private float moveY;
+    CharacterController characterController;
+    private Vector3 moveDirection = Vector3.zero;
+    public int speed;
+
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+    void Update()
+    {
+        moveX = Input.GetAxis("Horizontal");
+        moveY = Input.GetAxis("Vertical");
+        moveDirection = new Vector3(Input.GetAxis("Horizontal") * speed, Input.GetAxis("Vertical") * speed, 0.0f);
+        characterController.Move(moveDirection * Time.deltaTime);
+    }
+}
