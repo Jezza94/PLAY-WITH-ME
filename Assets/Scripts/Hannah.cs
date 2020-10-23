f80e74d5615216635d38f7fe1a0805c1fa4dcfc2
sinisteroglop <73212825+sinisteroglop@users.noreply.github.com>
2020-10-23 02:59:58 -0400

    Add files via upload

--- Assets/Scripts/Hannah.cs
+++ Assets/Scripts/Hannah.cs
@@ -10,8 +10,14 @@ public class Hannah : MonoBehaviour
     private Vector3 moveDirection = Vector3.zero;
     public int speed;
 
+    //public GameObject invSlot1; // The UI display for the first item in the inventory.
+    //public GameObject invSlot2;
+    //bool[] hasItem = new bool[7]; // Boolean array containing inventory data. Defaults to false. Each element of the array represents 1 inventory slot.
+    public AudioSource doorCreak;
+
     //the following GameObjects are doors that can be 'Unlocked' whenever a key with a respective tag is picked up. Add more when
     //more doors become a thing.
+    public GameObject SomethingUnlocked;
     public GameObject door1;
     public GameObject door2;
     public GameObject door3;
@@ -21,6 +27,9 @@ public class Hannah : MonoBehaviour
     void Start()
     {
         characterController = GetComponent<CharacterController>();
+        SomethingUnlocked.SetActive(false);
+        // invSlot1.SetActive(false); //Hides display before item is added to inventory.
+        //invSlot2.SetActive(false);
     }
     void Update()
     {
@@ -28,6 +37,8 @@ public class Hannah : MonoBehaviour
         moveY = Input.GetAxis("Vertical");
         moveDirection = new Vector3(Input.GetAxis("Horizontal") * speed, Input.GetAxis("Vertical") * speed, 0.0f);
         characterController.Move(moveDirection * Time.deltaTime);
+        //invSlot1.SetActive(hasItem[0]); //Checks every frame for item in inventory and displays it if the boolean is true.
+        //invSlot2.SetActive(hasItem[1]);
     }
 
 
@@ -39,26 +50,50 @@ public class Hannah : MonoBehaviour
         {
             Destroy(col.gameObject);
             door1.SendMessage("Unlock");
-        }else if (col.gameObject.CompareTag("Key2"))
+            DoorTransition();
+        }
+        else if (col.gameObject.CompareTag("Key2"))
         {
             Destroy(col.gameObject);
             door2.SendMessage("Unlock");
+            DoorTransition();
         }
         else if (col.gameObject.CompareTag("Key3"))
         {
             Destroy(col.gameObject);
             door3.SendMessage("Unlock");
+            DoorTransition();
         }
         else if (col.gameObject.CompareTag("Key4"))
         {
             Destroy(col.gameObject);
             door4.SendMessage("Unlock");
+            DoorTransition();
         }
         else if (col.gameObject.CompareTag("Key5"))
         {
             Destroy(col.gameObject);
             door5.SendMessage("Unlock");
+            DoorTransition();
         }
+    }
+
+    public void DoorTransition()
+    {
+        doorCreak.Play();
+        SomethingUnlocked.SetActive(true);
+        RemoveText();
+    }
+
+    void RemoveText()
+    {
+        StartCoroutine(RemoveAfterSeconds(3, SomethingUnlocked));
+    }
+
+    IEnumerator RemoveAfterSeconds(int seconds, GameObject obj)
+    {
+        yield return new WaitForSeconds(seconds);
+        obj.SetActive(false);
     }
 
 
