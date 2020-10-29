ff2cafea8702eed17761059f7708e26706166c28
Jeremy <jkl.looney@gmail.com>
2020-10-28 17:57:50 -0700

    Alpha Build Update Attempt

---
+++ PLAY-WITH-ME-main/Assets/Scripts/CharacterScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterScript : MonoBehaviour
+{
+
+    public GameObject invSlot1; // The UI display for the first item in the inventory.
+    public GameObject invSlot2;
+    bool[] hasItem = new bool[7]; // Boolean array containing inventory data. Defaults to false. Each element of the array represents 1 inventory slot.
+    public AudioSource doorCreak;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        invSlot1.SetActive(false); //Hides display before item is added to inventory.
+        invSlot2.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        invSlot1.SetActive(hasItem[0]); //Checks every frame for item in inventory and displays it if the boolean is true.
+        invSlot2.SetActive(hasItem[1]);
+    }
+
+    public void DoorTransition()
+    {
+        doorCreak.Play();
+    }
+}
